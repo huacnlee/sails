ac5e9f977f57dab8f86b149ced1232f866f9e7b8
Jason Lee <huacnlee@gmail.com>
2014-12-09 10:29:03 +0800

    Update class documents

--- lib/sails/base.rb
+++ lib/sails/base.rb
@@ -7,6 +7,24 @@ require 'yaml'
 
 Bundler.require()
 
+# Sails 
+#
+# You can custom Sails configs in config/application.rb
+#
+#    module Sails
+#      config.app_name = 'you_app_name'
+#      config.thrift_host = "127.0.0.1"
+#      config.thrift_port = 7075
+#      config.thrift_processor = Thrift::YouAppName::Processor
+#    
+#      config.autoload_paths += %W(app/workers)
+#    
+#      config.i18n.default_locale = 'zh-CN'
+#    
+#      # cache store
+#      config.cache_store = [:dalli_store, '127.0.0.1' }]
+#    end
+#
 module Sails
   extend ActiveSupport::Autoload
 
@@ -14,6 +32,20 @@ module Sails
     include ActiveSupport::Configurable
   end
 
+  # Sails.config
+  #
+  # Configs with Sails
+  # For example:
+  #
+  #   Sails.config.app_name
+  #   # => returns "You App Name"
+  #
+  #   Sails.config.autoload_paths
+  #   # => returns ['app/models','app/models/concerns', 'app/workers', 'app/services'...]
+  #
+  #   Sails.config.cache_store = [:dalli_store, '127.0.0.1', { pool_size: 100 }]
+  #   Sails.config.i18n.default_locale = 'zh-CN'
+  #
   def self.config
     return @config if defined?(@config)
     @config = Config.new.config
@@ -27,12 +59,46 @@ module Sails
     @config
   end
 
+  # Sails.cache
+  #
+  # An abstract cache store class. There are multiple cache store
+  # implementations, each having its own additional features. See the classes
+  # under the ActiveSupport::Cache module, e.g.
+  # ActiveSupport::Cache::MemCacheStore. MemCacheStore is currently the most
+  # popular cache store for large production websites.
+  #
+  # Some implementations may not support all methods beyond the basic cache
+  # methods of +fetch+, +write+, +read+, +exist?+, and +delete+.
+  #
+  # ActiveSupport::Cache::Store can store any serializable Ruby object.
+  #
+  #   Sails.cache.read('city')   # => nil
+  #   Sails.cache.write('city', "Duckburgh")
+  #   Sails.cache.read('city')   # => "Duckburgh"
+  #
+  # Keys are always translated into Strings and are case sensitive. When an
+  # object is specified as a key and has a +cache_key+ method defined, this
+  # method will be called to define the key.  Otherwise, the +to_param+
+  # method will be called. Hashes and Arrays can also be used as keys. The
+  # elements will be delimited by slashes, and the elements within a Hash
+  # will be sorted by key so they are consistent.
+  #
+  #   Sails.cache.read('city') == Sails.cache.read(:city)   # => true
+  #
+  # Nil values can be cached.
   def self.cache
     return @cache if defined?(@cache)
     @cache = ActiveSupport::Cache.lookup_store(self.config.cache_store)
   end
 
   # Sails.root
+  #
+  # This method returns a Pathname object which handles paths starting with a / as absolute (starting from the root of the filesystem). Compare:
+  # For example:
+  #   >> Sails.root
+  #   => #<Pathname:/some/path/to/project>
+  #   >> Sails.root + "file"
+  #   => #<Pathname:/some/path/to/project/file>
   def self.root
     @root ||= Pathname.new(Dir.pwd)
   end
@@ -41,11 +107,28 @@ module Sails
     @root = Pathname.new(root)
   end
 
-  # Sails.env.development?
+  # Sails.env
+  #
+  # returns a string representing the current Sails environment.
+  # This will read from ENV['RAILS_ENV'] like Rails
+  # For example:
+  #
+  #     Sails.env # in development mode
+  #     => "development"
+  #     Sails.env.development?
+  #     => true
+=> "development"
   def self.env
     @env ||= ActiveSupport::StringInquirer.new(ENV['RAILS_ENV'].presence || 'development')
   end
 
+  # Sails.logger
+  #
+  # returns a Logger class
+  # For example:
+  #
+  #     Sails.logger.info "Hello world"
+  #     Sails.logger.error "Hello world"
   def self.logger
     return @logger if defined?(@logger)
     @logger = Logger.new(File.join(Sails.root, "log/#{self.env}.log"))
@@ -54,21 +137,6 @@ module Sails
       "#{msg}\n"
     }
     @logger
-  end
-
-  def self.stdout_logger
-    return @stdout_logger if defined?(@stdout_logger)
-    @stdout_logger = Logger.new(STDOUT)
-    @stdout_logger.formatter = proc { |severity, datetime, progname, msg|
-      "#{msg}\n"
-    }
-    @stdout_logger
-  end
-
-  def self.load_initialize
-    Dir["#{Sails.root}/config/initializers/*.rb"].each do |f|
-      require f
-    end
   end
 
   def self.init
@@ -150,6 +218,22 @@ module Sails
         sleep 2
         retry
       end
+    end
+  end
+  
+  private
+  def self.stdout_logger
+    return @stdout_logger if defined?(@stdout_logger)
+    @stdout_logger = Logger.new(STDOUT)
+    @stdout_logger.formatter = proc { |severity, datetime, progname, msg|
+      "#{msg}\n"
+    }
+    @stdout_logger
+  end
+
+  def self.load_initialize
+    Dir["#{Sails.root}/config/initializers/*.rb"].each do |f|
+      require f
     end
   end
 end
